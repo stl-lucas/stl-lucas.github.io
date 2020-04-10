a6c9fada4a6ed07b418a60dde17d4cb814e197a7
Tim <tllucas1s@gmail.com>
2020-04-09 19:18:38 -0500

    Changed the .html file to .php and added some basic profile content.

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Resume and Portfolio">
+	<meta name="keywords" content="Resume,Portfolio,Java,C#,Python,SQL,Mongo,DB,Flask,.NET,HTML,CSS,XML,JavaScript">
+	<meta name="author" content="Timothy L. Lucas">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="styles/css/bootstrap.min.css" rel="stylesheet"/>
+	<script src="styles/fontawesome/svg-with-js/js/fontawesome-all.js"></script>
+	<script src="scripts/js/bootstrap.bundle.min.js"></script>
+	
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113096343-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-113096343-1');
+	</script>
+
+
+</head>
+<body id="top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#top">Timothy L. Lucas</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#resume">Resume</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+	
+	
+	<header class="bg-primary text-white">
+		<div class="container text-center">
+		<p><br/><br/></p>
+		</div>
+	</header>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-4">
+			<h1>Hi, I'm Tim!</h1>
+			<p class="lead">It's great to meet you and I just want to say thanks for stopping by!</p>
+			<p class="lead">If you have an exciting project or opportunity I can help with please let me know.</p>
+			<p class="lead">Feel free to connect with me on linked in or send me your contact info below.</p>
+			</div>
+			<div class="col-md-4">
+				<img src="img/profile2.jpg" width="400px" class="rounded" alt="profile picture">
+				<p></p>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+		
+	<section id="resume">
+		<section class="jumbotron text-center">
+			<div class="container">
+			  <h1 class="jumbotron-heading">Resume</h1>
+			  <p class="lead text-muted">You can find a PDF version of my resume <a href="img/resume.pdf" target="_blank">here</a> as well.</p>
+			</div>
+		</section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-12">
+					<h3>OBJECTIVE</h3>
+					<hr/>
+					<p>I'm enjoying my current position as Senior Product Manager with Teknowlogi. My hope is to continue networking and developing my skills as a data scientist. I enjoy solving problems and working with other passionate people. Let me know if there is a project we can work on together.</p>
+					<h3>EDUCATION</h3>
+					<hr/>
+					<h5><strong>MASTERS, IT MANAGEMENT</strong></h5>
+					<h6><em>Webster University</em></h6>
+					<h6>Saint Louis, MO</h6>
+					<h6>May 2016 - 3.9 GPA</h6>
+					<h5><strong>BACHELORS, MASS COMMUNICATIONS</strong></h5>
+					<h6><em>Southeast Missouri State University</em></h6>
+					<h6>Cape Girardeau, MO</h6>
+					<h6>May 2010 - 3.3 GPA</h6>
+					<h3>CERTIFICATIONS</h3>
+					<hr/>
+					<h5><a href="https://bootcamp.tlcenter.wustl.edu/data/" target="_blank"><strong>Wash University Data Analytics Boot Camp - Expected Jun 2020</strong></a></h5>
+					<h5><a href="https://www.launchcode.org/lc101" target="_blank"><strong>LaunchCode 101 - Jan 2018</strong></a></h5>
+				</div>
+				
+				<div class="col-md-8 col-sm-12">
+					<h3>EXPERIENCE</h3>
+					<hr/>
+					<h5><strong>SENIOR PRODUCT MANAGER - TEKNOWLOGI</strong></h5>
+					<h6><em>AUGUST 2019 - CURRENT</em></h6>
+					<p>Costa Mesa, CA - I'm currently managing our newest product. An AI-based tool designed to analyze customer data, advise on possible issues or opportunities and provide execution strategies to take immediate action. I'm creating data analyzers using data scripts and Machine Learning, writing UI based advising cards using a combination of data analytics, business intelligence, and consulting intelligence, and deciding on execution workflows that will automate customer action via API into their TMS.</p>
+					<h5><strong>SENIOR TECHNICAL SOLUTIONS SPECIALIST - TEKNOWLOGI</strong></h5>
+					<h6><em>SEPTEMBER 2015 - AUGUST 2019</em></h6>
+					<p>Costa Mesa, CA - Served as lead Implementation Manager responsible for onboarding all new customers. I've successfully implemented 125+ customers in four years with over a 90% success and retention rate. I also served on our support team, as QA Tester, EDI Specialist, and Training guru.</p>
+					<h5><strong>DIRECTOR OF OPERATIONS - PPLUS GLOBAL LOGISTICS</strong></h5>
+					<h6><em>AUGUST 2010 - SEPTEMBER 2015</em></h6>
+					<p>Saint Charles, MO - As the manager to a team of 10 - 13 employees with annual goals exceeding $2 million in revenue, I was our in-office leader and driver of all daily activity. I was responsible for forecasting customer trends and strategic planning initiatives reporting directly to the president. Built all company websites, training manuals, handbooks, and internal IT infrastructure.</p>
+					<h3>SKILLS</h3>
+					<hr/>
+					<h5><strong>Good</strong></h5>
+					<p>Java, SpringBoot, C#, .NET, JavaScript</p>
+					<h5><strong>Better</strong></h5>
+					<p>MongoDB, PHP, Flask, Tableau, SoapUI, Swagger, API, EDI</p>
+					<h5><strong>Best</strong></h5>
+					<p>Python, SQL, Excel, VBA, HTML, CSS, Bootstrap, Asana, Jira, Agile</p>
+				</div>
+			</div>
+		</div>
+	</section>
+		
+	<section id="portfolio">
+		<section class="jumbotron text-center">
+			<div class="container">
+			  <h1 class="jumbotron-heading">Portfolio</h1>
+			  <p class="lead text-muted">Here are some examples of my past work. Some for work and others for fun.</p>
+			</div>
+		</section>
+		<div class="album py-5 bg-light">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+					  <div class="card mb-4 box-shadow">
+						<img class="card-img-top" src="img/freightquote.png" alt="Card image cap">
+						<div class="card-body">
+						  <p class="card-text">A form-based quoting tool that was contracted for a freight brokerage. Delivered a web-based solution for submission of quotes with the ability to track history, response time and a few other metrics.</p>
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <a href="http://freightquote.lucastimothy.com" class="btn btn-sm btn-outline-secondary" target="_blank">View</a>
+							</div>
+							<small class="text-muted">2017, PHP, Bootstrap, SQL</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div class="col-md-4">
+					  <div class="card mb-4 box-shadow">
+						<img class="card-img-top" src="img/spacewar.png" alt="Card image cap">
+						<div class="card-body">
+						  <p class="card-text">SpaceWar like retro arcade game. Developed using Construct for both web and mobile. P1 uses W, A, D to move and V to fire. P2 uses Up, Left, Right to movee and M to fire.</p>
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <a href="http://spacewar.lucastimothy.com" class="btn btn-sm btn-outline-secondary" target="_blank">View</a>
+							</div>
+							<small class="text-muted">2017, Construct</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+					<div class="col-md-4">
+					  <div class="card mb-4 box-shadow">
+						<img class="card-img-top" src="img/pong.png" alt="Card image cap">
+						<div class="card-body">
+						  <p class="card-text">Simple take on the classic game, Pong. Developed using Construct for both web and mobile. Move with Up and Down arrows. Touch controls also available.</p>
+						  <div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+							  <a href="http://pong.lucastimothy.com" class="btn btn-sm btn-outline-secondary" target="_blank">View</a>
+							</div>
+							<small class="text-muted">2017, Construct</small>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+		
+	<section id="contact">
+		<section class="jumbotron">
+			<div class="container">
+			  <h1 class="jumbotron-heading text-center">Contact</h1>
+			  <p class="lead text-muted text-center">I'd love to hear from you!</p>
+				<div class="row">
+					<div class="col-md-3"></div>
+					<div class="col-md-6">
+						<form action="contactform.php" method="post">
+						  <div class="form-group">
+							<label for="sender">Email address</label>
+							<input type="email" class="form-control" id="sender" name="mail" placeholder="Enter email">
+						  </div>
+						  <div class="form-group">
+							<label for="reason">Subject</label>
+							<input type="text" class="form-control" id="reason" name="subject" placeholder="I'd love to meet you.">
+						  </div>
+						  <div class="form-group">
+							<label for="body">Reason for Contact</label>
+							<textarea type="text" class="form-control" id="body" name="message" placeholder="We're looking for someone with your skillset to help with an awesome project."></textarea>
+						  </div>
+						  <button type="submit" name="submit" class="btn btn-primary btn-lg btn-block">Send!</button>
+						</form>
+					</div>
+					<div class="col-md-3"></div>
+				</div>
+			</div>
+		</section>
+	</section>		
+	
+	<section id="footer">
+		<footer>
+			<div class="container">
+			  <div class="lead text-muted float-left">&copy; <?php echo date("Y");?> Timothy L. Lucas | All Rights Reserved.</div>
+			  <div class="float-right"><a href="tel:1-314-440-8514" target="_blank"><i class="fas fa-phone fa-2x"></i></a>&nbsp;&nbsp;<a href="mailto:lucas.timothy@icloud.com"><i class="fas fa-envelope fa-2x"></i></a>&nbsp;&nbsp;<a href="http://www.linkedin.com/in/lucas-timothy" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a></div>
+			</div>
+		</footer>
+	</section>
+</body>
+</html>
